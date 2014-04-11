ff9785eb97b8d3d3c9a201f98834d3af1d439e9f
kaciano <kaciano@kaciano-SERH>
2014-04-11 11:36:50 -0300

    Novo componente adicionado e iniciada a construção do método de limpeza MOTHERFUCKER

---
+++ GMPComps/src/main/java/br/com/gmp/comps/cleaner/ComponentCleaner.java
@@ -0,0 +1,74 @@
+package br.com.gmp.comps.cleaner;
+
+import java.awt.Component;
+import java.awt.Container;
+import javax.swing.JCheckBox;
+import javax.swing.text.JTextComponent;
+
+/**
+ * Classe utilitária para limpar os valores de todos os componentes
+ *
+ * @author kaciano
+ */
+public class ComponentCleaner {
+
+    private boolean cleanEverything;
+
+    public ComponentCleaner(boolean cleanEverything) {
+        this.cleanEverything = cleanEverything;
+    }
+
+    /**
+     * Método de limpeza principal
+     *
+     * @param c <code>Container</code> Container raiz
+     */
+    public void clean(Container c) {
+        System.out.println("Raiz: " + c.getName());
+        for (Component comp : c.getComponents()) {
+            if (c instanceof Container) {
+                Container cont = (Container) comp;
+                clean(cont);
+                cleanComponents(cont);
+            }
+        }
+        cleanComponents(c);
+    }
+
+    /**
+     * Método de limpeza dos componentes internos
+     *
+     * @param c <code>Container</code> Container raiz
+     */
+    public void cleanComponents(Container c) {
+        for (Component comp : c.getComponents()) {
+            System.out.println("Verificando " + comp.getName());
+            if (comp instanceof JTextComponent) {
+                JTextComponent txt = (JTextComponent) comp;
+                txt.setText("");
+            } else if (comp instanceof JCheckBox) {
+                JCheckBox check = (JCheckBox) comp;
+                check.setSelected(false);
+            }
+        }
+    }
+
+    /**
+     * Deve limpar tudo?
+     *
+     * @return Boolean Limpar tudo
+     */
+    public Boolean isCleanEverything() {
+        return cleanEverything;
+    }
+
+    /**
+     * Modifica limpar tudo
+     *
+     * @param cleanEverything Boolean Limpar tudo
+     */
+    public void setCleanEverything(boolean cleanEverything) {
+        this.cleanEverything = cleanEverything;
+    }
+
+}
