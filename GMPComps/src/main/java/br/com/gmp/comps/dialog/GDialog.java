65c05029ccb9fef5572aa327363d472abce54378
kaciano <kaciano@kaciano-K55A>
2014-04-20 23:34:30 -0300

    Correção no GlassPane do GDialog

--- GMPComps/src/main/java/br/com/gmp/comps/dialog/GDialog.java
+++ GMPComps/src/main/java/br/com/gmp/comps/dialog/GDialog.java
@@ -274,23 +274,26 @@ public class GDialog extends javax.swing.JDialog {
         if (getParent() instanceof JFrame) {
             JFrame p = (JFrame) getParent();
             setLocationRelativeTo(p);
-            p.setGlassPane(new BlurGlassPane());
+            p.setGlassPane(state ? new BlurGlassPane() : new JFrame().getGlassPane());
             p.getGlassPane().setVisible(state);
         } else if (getParent() instanceof JInternalFrame) {
             JInternalFrame p = (JInternalFrame) getParent();
             setLocationRelativeTo(p);
-            p.setGlassPane(new BlurGlassPane());
+            p.setGlassPane(state ? new BlurGlassPane() : new JInternalFrame().getGlassPane());
             p.getGlassPane().setVisible(state);
         } else if (getParent() instanceof JDialog) {
             JDialog p = (JDialog) getParent();
             setLocationRelativeTo(p);
-            p.setGlassPane(new BlurGlassPane());
+            p.setGlassPane(state ? new BlurGlassPane() : new JDialog().getGlassPane());
             p.getGlassPane().setVisible(state);
         } else if (getParent() instanceof JWindow) {
             JWindow p = (JWindow) getParent();
             setLocationRelativeTo(p);
-            p.setGlassPane(new BlurGlassPane());
+            p.setGlassPane(state ? new BlurGlassPane() : new JWindow().getGlassPane());
             p.getGlassPane().setVisible(state);
+        }
+        if (!state) {
+            
         }
     }
 
