1e55b409bce1eb46f37a59fe837bba49149b3a3c
kaciano <kaciano@kaciano-SERH>
2014-04-30 16:29:00 -0300

    Adicionadas variaveis estaticas para coloraçao de texto no console

--- GMPUtils/src/main/java/br/com/gmp/utils/system/SystemProperties.java
+++ GMPUtils/src/main/java/br/com/gmp/utils/system/SystemProperties.java
@@ -16,6 +16,14 @@ public interface SystemProperties {
     public String FILE_ENCODING = System.getProperties().getProperty("file.encoding");
     public String FILE_SEPARATOR = System.getProperties().getProperty("file.separator");
     public String JAVA_RUNTIME_VERSION = System.getProperties().getProperty("java.runtime.version");
-    public String JAVA_VERSION = System.getProperties().getProperty("java.version");    
-    
+    public String JAVA_VERSION = System.getProperties().getProperty("java.version");
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
 }
