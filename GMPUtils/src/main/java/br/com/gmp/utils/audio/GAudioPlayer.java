e1be8678a987b61fbaaec33f40687539c1609c15
Kaciano <kaciano.ghelere@serhmatica.com.br>
2014-06-13 08:44:00 -0300

    Javadoc atualizado

--- GMPUtils/src/main/java/br/com/gmp/utils/audio/GAudioPlayer.java
+++ GMPUtils/src/main/java/br/com/gmp/utils/audio/GAudioPlayer.java
@@ -17,12 +17,14 @@ import javazoom.jl.player.AudioDevice;
 import javazoom.jl.player.FactoryRegistry;
 
 /**
+ * Controlador de reprodução de audio
  *
  * @author kaciano
+ * @version 1.0
  */
 public class GAudioPlayer {
 
-    private URL urlToStreamFrom;
+    private URL urlStream;
     private String audioPath;
     private Bitstream bitstream;
     private Decoder decoder;
@@ -35,16 +37,33 @@ public class GAudioPlayer {
     private int frameIndexCurrent;
     private final int lostFrames = 20;
 
-    public GAudioPlayer(URL urlToStreamFrom) throws JavaLayerException {
-        this.urlToStreamFrom = urlToStreamFrom;
+    /**
+     * Cria nova instancia de GAudioPlayer
+     *
+     * @param urlStream <code>URL</code> URL do Stream
+     * @throws JavaLayerException Exceção de JavaLayer
+     */
+    public GAudioPlayer(URL urlStream) throws JavaLayerException {
+        this.urlStream = urlStream;
         this.listener = new PlaybackAdapter();
     }
 
+    /**
+     * Cria nova instancia de GAudioPlayer
+     *
+     * @param audioPath <code>String</code> Caminho do arquivo
+     * @throws JavaLayerException Exceção de JavaLayer
+     */
     public GAudioPlayer(String audioPath) throws JavaLayerException {
         this.audioPath = audioPath;
         this.listener = new PlaybackAdapter();
     }
 
+    /**
+     * Modifica o PlaybackListener
+     *
+     * @param newPlaybackListener <code>PlaybackListener</code> PlaybackListener
+     */
     public void setPlaybackListener(PlaybackListener newPlaybackListener) {
         if (newPlaybackListener != null) {
             this.listener = newPlaybackListener;
@@ -53,23 +72,90 @@ public class GAudioPlayer {
         }
     }
 
+    /**
+     * Retorna o stream de audio
+     *
+     * @return <code>InputStream</code> Stream de audio
+     * @throws IOException Exceção de I/O
+     */
     private InputStream getAudioInputStream() throws IOException {
         if (this.audioPath != null) {
             return new FileInputStream(this.audioPath);
-        } else if (this.urlToStreamFrom != null) {
-            this.urlToStreamFrom.openStream();
+        } else if (this.urlStream != null) {
+            this.urlStream.openStream();
         }
         return null;
     }
 
+    /**
+     * Decodifica frames
+     *
+     * @return <code>boolean</code> Frame decodificado?
+     * @throws JavaLayerException Exceção de JavaLayer
+     */
+    protected boolean decodeFrame() throws JavaLayerException {
+        boolean returnValue = false;
+        if (this.stopped) {
+            return false;
+        }
+
+        try {
+            if (this.audioDevice != null) {
+                Header header = this.bitstream.readFrame();
+                if (header != null) {
+                    SampleBuffer output = (SampleBuffer) this.decoder
+                            .decodeFrame(header, this.bitstream);
+
+                    synchronized (this) {
+                        if (this.audioDevice != null) {
+                            this.audioDevice.write(output.getBuffer(), 0,
+                                    output.getBufferLength());
+                        }
+                    }
+
+                    this.bitstream.closeFrame();
+                    returnValue = true;
+                } else {
+                    System.out.println("End of file");
+                    returnValue = false;
+                }
+            }
+        } catch (RuntimeException ex) {
+            throw new JavaLayerException("Exception decoding audio frame", ex);
+        }
+        return returnValue;
+    }
+
+    /**
+     * Inicia a reprodução de audio
+     *
+     * @return <code>boolean</code> Reprodução iniciada?
+     * @throws JavaLayerException Exceção de JavaLayer
+     */
     public boolean play() throws JavaLayerException {
         return this.play(0);
     }
 
+    /**
+     * Inicia a reprodução de audio
+     *
+     * @param frameIndexStart <code>int</code> Frame inicial
+     * @return <code>boolean</code> Reprodução iniciada?
+     * @throws JavaLayerException Exceção de JavaLayer
+     */
     public boolean play(int frameIndexStart) throws JavaLayerException {
         return this.play(frameIndexStart, -1, lostFrames);
     }
 
+    /**
+     * Inicia a reprodução de audio
+     *
+     * @param frameIndexStart <code>int</code> Frame inicial
+     * @param frameIndexFinal <code>int</code> Frame final
+     * @param correctionFactorInFrames <code>int</code> Fator de correção
+     * @return <code>boolean</code> Reprodução iniciada?
+     * @throws JavaLayerException Exceção de JavaLayer
+     */
     public boolean play(int frameIndexStart, int frameIndexFinal,
             int correctionFactorInFrames) throws JavaLayerException {
         try {
@@ -145,10 +231,19 @@ public class GAudioPlayer {
         return shouldContinueReadingFrames;
     }
 
+    /**
+     * Reinicia a reprodução do audio
+     *
+     * @return <code>boolean</code> Reprodução em execução?
+     * @throws JavaLayerException Exceção de JavaLayer
+     */
     public boolean resume() throws JavaLayerException {
         return this.play(this.frameIndexCurrent);
     }
 
+    /**
+     * Fecha a reprodução de audio
+     */
     public synchronized void close() {
         if (this.audioDevice != null) {
             this.closed = true;
@@ -165,39 +260,9 @@ public class GAudioPlayer {
         }
     }
 
-    protected boolean decodeFrame() throws JavaLayerException {
-        boolean returnValue = false;
-        if (this.stopped) {
-            return false;
-        }
-
-        try {
-            if (this.audioDevice != null) {
-                Header header = this.bitstream.readFrame();
-                if (header != null) {
-                    SampleBuffer output = (SampleBuffer) this.decoder
-                            .decodeFrame(header, this.bitstream);
-
-                    synchronized (this) {
-                        if (this.audioDevice != null) {
-                            this.audioDevice.write(output.getBuffer(), 0,
-                                    output.getBufferLength());
-                        }
-                    }
-
-                    this.bitstream.closeFrame();
-                    returnValue = true;
-                } else {
-                    System.out.println("End of file");
-                    returnValue = false;
-                }
-            }
-        } catch (RuntimeException ex) {
-            throw new JavaLayerException("Exception decoding audio frame", ex);
-        }
-        return returnValue;
-    }
-
+    /**
+     * Pausa a execução
+     */
     public void pause() {
         if (!this.stopped) {
             this.paused = true;
@@ -210,18 +275,9 @@ public class GAudioPlayer {
         }
     }
 
-    protected boolean skipFrame() throws JavaLayerException {
-        boolean returnValue = false;
-        Header header = this.bitstream.readFrame();
-
-        if (header != null) {
-            this.bitstream.closeFrame();
-            returnValue = true;
-        }
-
-        return returnValue;
-    }
-
+    /**
+     * Para a reprodução
+     */
     public void stop() {
         if (!this.stopped) {
             if (!this.closed) {
@@ -239,76 +295,195 @@ public class GAudioPlayer {
         }
     }
 
+    /**
+     * Pula frames
+     *
+     * @return <code>boolean</code> Pulou o frame?
+     * @throws JavaLayerException Exceção de JavaLayer
+     */
+    protected boolean skipFrame() throws JavaLayerException {
+        boolean returnValue = false;
+        Header header = this.bitstream.readFrame();
+
+        if (header != null) {
+            this.bitstream.closeFrame();
+            returnValue = true;
+        }
+
+        return returnValue;
+    }
+
+    /**
+     * Retorna se está terminado
+     *
+     * @return <code>boolean</code> Está terminado?
+     */
     public boolean isClosed() {
         return closed;
     }
 
+    /**
+     * Retorna se está completo
+     *
+     * @return <code>boolean</code> Está completo?
+     */
     public boolean isComplete() {
         return complete;
     }
 
+    /**
+     * Retorna se está em pausa
+     *
+     * @return <code>boolean</code> Está em pausa?
+     */
     public boolean isPaused() {
         return paused;
     }
 
+    /**
+     * Retorna se está parado
+     *
+     * @return <code>boolean</code> Está parado?
+     */
     public boolean isStopped() {
         return stopped;
     }
 
+    /**
+     * Evento para playback listeners
+     */
     public static class PlaybackEvent {
 
+        /**
+         * Fonte de audio
+         */
         public GAudioPlayer source;
+
+        /**
+         * Tipo do evento
+         */
         public EventType eventType;
+
+        /**
+         * Endereço do frame
+         */
         public int frameIndex;
 
-        public PlaybackEvent(GAudioPlayer source, EventType eventType,
-                int frameIndex) {
+        /**
+         * Cria nova instancia de PlaybackEvent
+         *
+         * @param source <code>GAudioPlayer</code> Fonte de audio
+         * @param eventType <code>EventType</code> Tipo do evento
+         * @param frameIndex <code>int</code> Endereço do frame
+         */
+        public PlaybackEvent(GAudioPlayer source, EventType eventType, int frameIndex) {
             this.source = source;
             this.eventType = eventType;
             this.frameIndex = frameIndex;
         }
 
+        /**
+         * Classe estatica dos tipos de evento
+         */
         public static class EventType {
 
+            /**
+             * Nome do tipo
+             */
             public String name;
 
+            /**
+             * Cria nova instancia de EventType
+             *
+             * @param name <code>String</code> Nome do tipo
+             */
             public EventType(String name) {
                 this.name = name;
             }
 
+            /**
+             * Classe estatica das instancias dos tipos de evento
+             */
             public static class Instances {
 
+                /**
+                 * Iniciado
+                 */
                 public static EventType Started = new EventType("Started");
+
+                /**
+                 * Em pausa
+                 */
                 public static EventType Paused = new EventType("Paused");
+
+                /**
+                 * Parado
+                 */
                 public static EventType Stopped = new EventType("Stopped");
             }
         }
     }
 
+    /**
+     * Adaptador de listener de playback
+     */
     public static class PlaybackAdapter implements PlaybackListener {
 
+        /**
+         * Método de inicio da execução de faixa
+         *
+         * @param event <code>PlaybackEvent</code> Evento de playback
+         */
         @Override
         public void playbackStarted(PlaybackEvent event) {
             System.err.println("Playback started");
         }
 
+        /**
+         * Método de pausa da execução de faixa
+         *
+         * @param event <code>PlaybackEvent</code> Evento de playback
+         */
         @Override
         public void playbackPaused(PlaybackEvent event) {
             System.err.println("Playback paused");
         }
 
+        /**
+         * Método de fim da execução de faixa
+         *
+         * @param event <code>PlaybackEvent</code> Evento de playback
+         */
         @Override
         public void playbackFinished(PlaybackEvent event) {
             System.err.println("Playback stopped");
         }
     }
 
+    /**
+     * Listener de playback
+     */
     public static interface PlaybackListener {
 
+        /**
+         * Método de inicio da execução de faixa
+         *
+         * @param event <code>PlaybackEvent</code> Evento de playback
+         */
         public void playbackStarted(PlaybackEvent event);
 
+        /**
+         * Método de pausa da execução de faixa
+         *
+         * @param event <code>PlaybackEvent</code> Evento de playback
+         */
         public void playbackPaused(PlaybackEvent event);
 
+        /**
+         * Método de fim da execução de faixa
+         *
+         * @param event <code>PlaybackEvent</code> Evento de playback
+         */
         public void playbackFinished(PlaybackEvent event);
     }
 }
--- GMPUtils/src/main/java/br/com/gmp/utils/audio/SoundLayer.java
+++ GMPUtils/src/main/java/br/com/gmp/utils/audio/SoundLayer.java
@@ -13,8 +13,10 @@ import org.jaudiotagger.tag.TagException;
 import org.jaudiotagger.tag.id3.ID3v1Tag;
 
 /**
+ * Reprodutor de audio
  *
  * @author kaciano
+ * @version 1.0
  */
 public class SoundLayer implements Runnable {
 
@@ -25,17 +27,31 @@ public class SoundLayer implements Runnable {
     private PlaybackListener playbackListener = new PlaybackListener();
     private ID3v1Tag tag;
 
+    /**
+     * Cria nova instancia de SoundLayer
+     *
+     * @param filePath <code>String</code> Caminho do arquivo
+     */
     public SoundLayer(String filePath) {
         this.filePath = filePath;
         loadTag();
     }
 
+    /**
+     * * Cria nova instancia de SoundLayer
+     *
+     * @param filePath <code>String</code> Caminho do arquivo
+     * @param namePlayerThread <code>String</code> Nome da Thread do Player
+     */
     public SoundLayer(String filePath, String namePlayerThread) {
         this.filePath = filePath;
         this.namePlayerThread = namePlayerThread;
         loadTag();
     }
 
+    /**
+     * Carrega os dados da faixa
+     */
     private void loadTag() {
         try {
             this.tag = new MP3File(new File(filePath)).getID3v1Tag();
@@ -50,22 +66,45 @@ public class SoundLayer implements Runnable {
                 + "\n---------------------------------------------------");
     }
 
+    /**
+     * Retorna o titutlo da faixa
+     *
+     * @return <code>String</code> Titulo da faixa
+     */
     public String getTitle() {
         return tag.getFirst(FieldKey.TITLE);
     }
 
+    /**
+     * Retorna o artista da faixa
+     *
+     * @return <code>String</code> Artista da faixa
+     */
     public String getArtist() {
         return tag.getFirst(FieldKey.ARTIST);
     }
 
+    /**
+     * Retorna o numero da faixa
+     *
+     * @return <code>String</code> Numero da faixa
+     */
     public String getTrack() {
         return tag.getFirst(FieldKey.TRACK);
     }
 
+    /**
+     * Retorna o da album faixa
+     *
+     * @return <code>String</code> Album da faixa
+     */
     public String getAlbum() {
         return tag.getFirst(FieldKey.ALBUM);
     }
 
+    /**
+     * Inicia reprodução
+     */
     public void play() {
         if (this.player == null) {
             this.playerInitialize();
@@ -79,6 +118,9 @@ public class SoundLayer implements Runnable {
         this.playerThread.start();
     }
 
+    /**
+     * Pausa reprodução
+     */
     public void pause() {
         if (this.player != null) {
             this.player.pause();
@@ -88,6 +130,9 @@ public class SoundLayer implements Runnable {
         }
     }
 
+    /**
+     * Troca o status de pausa
+     */
     public void pauseToggle() {
         if (this.player != null) {
             if (this.player.isPaused() && !this.player.isStopped()) {
@@ -98,6 +143,9 @@ public class SoundLayer implements Runnable {
         }
     }
 
+    /**
+     * Para reprodução
+     */
     public void stop() {
         if (this.player != null) {
             this.player.stop();
@@ -107,6 +155,9 @@ public class SoundLayer implements Runnable {
         }
     }
 
+    /**
+     * Inicializa o player
+     */
     private void playerInitialize() {
         try {
             this.player = new GAudioPlayer(this.filePath);
@@ -125,6 +176,9 @@ public class SoundLayer implements Runnable {
         }
     }
 
+    /**
+     * Listener privado para reprodução
+     */
     private static class PlaybackListener extends GAudioPlayer.PlaybackAdapter {
 
         @Override
@@ -143,6 +197,11 @@ public class SoundLayer implements Runnable {
         }
     }
 
+    /**
+     * Dados da faixa de audio
+     *
+     * @return <code>ID3v1Tag</code> Dados da faixa
+     */
     public ID3v1Tag getTag() {
         return tag;
     }
