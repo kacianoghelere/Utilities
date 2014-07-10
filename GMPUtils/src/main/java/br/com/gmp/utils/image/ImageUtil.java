93a93a365812456bd40d0eb962bc4fb4a6ae801e
Kaciano <kaciano.ghelere@serhmatica.com.br>
2014-07-10 11:08:19 -0300

    Adicionada função no utilitário de imagens

--- GMPUtils/src/main/java/br/com/gmp/utils/image/ImageUtil.java
+++ GMPUtils/src/main/java/br/com/gmp/utils/image/ImageUtil.java
@@ -4,11 +4,15 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
+import javax.swing.ImageIcon;
 
 /**
  * Coleção de métodos utilitários para trabalhar com imagens
  *
  * @author kaciano
+ * @version 1.0
+ * @author kaciano
+ * @version 1.1
  */
 public class ImageUtil {
 
@@ -19,6 +23,7 @@ public class ImageUtil {
      * @param w {@code int} Largura
      * @param h {@code int} Altura
      * @return {@code Image} Imagem redimensionada
+     * @since 1.0
      */
     public Image getScaledImage(Image srcImg, int w, int h) {
         BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
@@ -28,4 +33,16 @@ public class ImageUtil {
         g2.dispose();
         return resizedImg;
     }
+
+    /**
+     * Retorna o ImageIcon carregado
+     *
+     * @param path {@code String} Caminho do icone
+     * @return {@code ImageIcon} Icone recuperado
+     * @since 1.1
+     */
+    public ImageIcon getResource(String path) {
+        return new ImageIcon(getClass().getResource(path));
+    }
+
 }
