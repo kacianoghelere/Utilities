044c90ca532d1fdc432bf20f1593a41934829190
ka <ka@kaciano-K55A>
2014-09-28 15:49:44 -0300

    Implementando ajustes nos campos de lookup

---
+++ GMPUtils/src/main/java/br/com/gmp/utils/annotations/LookUp.java
@@ -0,0 +1,21 @@
+package br.com.gmp.utils.annotations;
+
+import java.lang.annotation.*;
+
+/**
+ * Interface de anotação para campos de LookUp
+ *
+ * @author kaciano
+ * @version 1.0
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface LookUp {
+
+    /**
+     * Retorna a classe do campo de LookUp
+     *
+     * @return {@code Class} Classe do campo
+     */
+    Class type();
+}
